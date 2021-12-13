66e1c2f5d2216e14eca0609c1712e648f463cd74
Shwetha Sreenivasa <shwetha@Shwethas-MacBook-Air.local>
2021-12-13 17:23:18 -0500

    added profile.js

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/childModel');
 
-//GET api/profile
+//GET logged in user profile api/profile/me
 //access Private
-router.get('/',(req,res) =>res.send('profile route'));
+router.get('/me',auth,async(req,res) =>{
+
+    try {
+       const profile = await User.Users.findById(req.user.id); 
+       if(!profile){
+           return res.status(400).json({msg:'No profile for this user'});
+       }
+       return res.json(profile);
+    } catch (error) {
+        console.log(error);
+    }
+});
 module.exports = router;
